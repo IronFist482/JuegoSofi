c6f95eedf447589eb13738b67fa52a9705ebb5fd
IronFist482 <113216659+IronFist482@users.noreply.github.com>
2022-11-27 17:50:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 io.on('connection', onConnection);
